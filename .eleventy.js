ea301a7699cb264eff435c8c11ac621ede6f8a49
Karol Sadkowski <karol.sadkowski@vanderbilt.edu>
2025-01-19 17:25:38 -0600

    Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
 module.exports = function(eleventyConfig) {
-    // Copy static assets to the output folder
-    eleventyConfig.addPassthroughCopy("src/css");
-  
-    return {
-      dir: {
-        input: "src",           // Tells Eleventy to use the `src/` folder
-        includes: "_includes",  // Folder for templates
-        output: "_site",        // Where static files are generated
-      },
-    };
+  // Copy static assets to the output folder
+  eleventyConfig.addPassthroughCopy("src/css");      // Copy CSS files
+  eleventyConfig.addPassthroughCopy("src/favicon.ico"); // Copy the favicon to the output folder
+
+  return {
+    dir: {
+      input: "src",           // Tells Eleventy to use the `src/` folder
+      includes: "_includes",  // Folder for templates
+      output: "_site",        // Where static files are generated
+    },
   };
+};
